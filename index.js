a1d626048d686240888d1b5b5030a730ac07bc66
Sam Perozek <samperozek@gmail.com>
2022-06-07 20:11:38 -0500

    commit tree f6ac405237f5395666c10bcca3fb63620a991327

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee = {name: "", streetAdress: "7031 Bryce Canyon"};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;    
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = Object.assign({},employee);
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employees,key){
+    delete employee[key];
+    return employee;
+}
